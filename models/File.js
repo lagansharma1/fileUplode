fd9228332d97c9cb2ea820d37a549299394299d0
JS16541 <JS16541@jeenasikholifecare.com>
2025-05-07 11:02:31 +0530

    nade mailer

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require ('mongoose');
+const nodemailer = require('nodemailer');
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -16,5 +17,32 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 })
+
+//post mi8ddleware
+// fileSchema.post('save',async function(doc){
+//     try{
+//         console.log("document",doc);
+//         let transporter = nodemailer.createTransport({
+//             host: process.env.MAIL_HOST,
+//             auth:{
+//                 user: process.env.MAIL_USER,
+//                 pass: process.env.MAIL_PASSWORD,
+//             }
+
+
+//         });
+//         let info = await transporter.sendMail({
+//             from: process.env.MAIL_FROM,
+//             to: doc.email,
+//             subject: "File Upload",
+//             text: `File ${doc.name} has been uploaded successfully`,
+//             html: `<b>File ${doc.name} has been uploaded successfully</b>`
+//         });
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
+
 const File = mongoose.model('File', fileSchema); 
 module.exports = File;
