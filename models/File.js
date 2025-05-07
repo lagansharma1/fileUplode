dbe87ce1565dff8fe02a57169fa8232158cf3e78
JS16541 <JS16541@jeenasikholifecare.com>
2025-05-07 14:08:40 +0530

    dfd

--- models/File.js
+++ models/File.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -20,27 +21,36 @@ const fileSchema = new mongoose.Schema({
 
 //post mi8ddleware
 // fileSchema.post('save',async function(doc){
-//     try{
-//         console.log("document",doc);
+//     try {
+//         // Ensure environment variables are loaded
+//         if (!process.env.MAIL_HOST || !process.env.MAIL_PORT || !process.env.MAIL_USER || !process.env.MAIL_PASSWORD) {
+//             throw new Error("Missing email configuration in environment variables.");
+//         }
+
+//         // Convert port to number if it's a string
+//         const port = parseInt(process.env.MAIL_PORT, 10);
+
 //         let transporter = nodemailer.createTransport({
 //             host: process.env.MAIL_HOST,
-//             auth:{
+//             port: port,
+//             secure: port === 465, // true for 465, false for other ports
+//             auth: {
 //                 user: process.env.MAIL_USER,
 //                 pass: process.env.MAIL_PASSWORD,
-//             }
-
-
+//             },
 //         });
+
 //         let info = await transporter.sendMail({
-//             from: process.env.MAIL_FROM,
+//             from: `"Uploader" <${process.env.MAIL_USER}>`,
 //             to: doc.email,
 //             subject: "File Upload",
 //             text: `File ${doc.name} has been uploaded successfully`,
-//             html: `<b>File ${doc.name} has been uploaded successfully</b>`
+//             html: `<b>File ${doc.name} has been uploaded successfully</b>`,
 //         });
-//     }
-//     catch(err){
-//         console.log(err);
+
+//         console.log("Email sent: %s", info.messageId);
+//     } catch (error) {
+//         console.error("Error sending email:", error);
 //     }
 // })
 
